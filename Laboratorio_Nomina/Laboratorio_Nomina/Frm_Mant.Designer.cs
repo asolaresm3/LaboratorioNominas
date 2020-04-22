10c2d3112bb2cefbfcdcc611ef39360948362b7a
asolaresm3 <52753087+asolaresm3@users.noreply.github.com>
2020-04-22 09:26:06 -0600

    Carga del MDI

---
+++ Laboratorio_Nomina/Laboratorio_Nomina/Frm_Mant.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Laboratorio_Nomina
+{
+    partial class Frm_Mant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador1 = new CapaDeDiseno.Navegador();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.Color.White;
+            this.navegador1.Location = new System.Drawing.Point(-2, -5);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(4);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(1571, 809);
+            this.navegador1.TabIndex = 0;
+            // 
+            // Frm_Mant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.navegador1);
+            this.Name = "Frm_Mant";
+            this.Text = "Frm_Mant";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CapaDeDiseno.Navegador navegador1;
+    }
+}
